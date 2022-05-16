1ae4dd2b52468dccc6af3a7c5031cac5d1b1b606
My2Labs <sking171@ford.com>
2022-05-16 11:13:33 -0500

    Add initial style.

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './components/head/head.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
