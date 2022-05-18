294d23e497e20743a745f0cf07ecba35b4534a12
My2Labs <sking171@ford.com>
2022-05-18 10:02:13 -0500

    Edit slideshow component.

--- FinalProject/src/app/components/slideshow/slideshow.component.ts
+++ FinalProject/src/app/components/slideshow/slideshow.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { photos } from '../../../pup-pictures';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css'],
 })
-export class SlideshowComponent {
+export class SlideshowComponent implements OnInit {
+  photos = photos;
+
   constructor() {}
 
-  onSlideChange(): void {
-    console.log('slide');
-  }
+  ngOnInit(): void {}
 }
---
+++ FinalProject/src/pup-pictures.ts
@@ -0,0 +1,667 @@
+export const photos = [
+  {
+    id: 1,
+    imageUrl: '/assets/pupaction1',
+    label: 'Action',
+  },
+  {
+    id: 2,
+    imageUrl: '/assets/pupaction2',
+    label: 'Action',
+  },
+  {
+    id: 3,
+    imageUrl: '/assets/pupaction3',
+    label: 'Action',
+  },
+  {
+    id: 4,
+    imageUrl: '/assets/pupaction4',
+    label: 'Action',
+  },
+  {
+    id: 5,
+    imageUrl: '/assets/pupaction5',
+    label: 'Action',
+  },
+  {
+    id: 6,
+    imageUrl: '/assets/pupaction6',
+    label: 'Action',
+  },
+  {
+    id: 7,
+    imageUrl: '/assets/pupaction7',
+    label: 'Action',
+  },
+  {
+    id: 8,
+    imageUrl: '/assets/pupaction8',
+    label: 'Action',
+  },
+  {
+    id: 9,
+    imageUrl: '/assets/pupaction9',
+    label: 'Action',
+  },
+  {
+    id: 10,
+    imageUrl: '/assets/pupaction10',
+    label: 'Action',
+  },
+  {
+    id: 11,
+    imageUrl: '/assets/pupaction11',
+    label: 'Action',
+  },
+  {
+    id: 12,
+    imageUrl: '/assets/pupaction12',
+    label: 'Action',
+  },
+  {
+    id: 13,
+    imageUrl: '/assets/pupaction13',
+    label: 'Action',
+  },
+  {
+    id: 14,
+    imageUrl: '/assets/pupaction14',
+    label: 'Action',
+  },
+  {
+    id: 15,
+    imageUrl: '/assets/pupaction15',
+    label: 'Action',
+  },
+  {
+    id: 16,
+    imageUrl: '/assets/pupaction16',
+    label: 'Action',
+  },
+  {
+    id: 17,
+    imageUrl: '/assets/pupcamp1',
+    label: 'Camping',
+  },
+  {
+    id: 18,
+    imageUrl: '/assets/pupcamp2',
+    label: 'Camping',
+  },
+  {
+    id: 19,
+    imageUrl: '/assets/pupcamp3',
+    label: 'Camping',
+  },
+  {
+    id: 20,
+    imageUrl: '/assets/pupcamp4',
+    label: 'Camping',
+  },
+  {
+    id: 21,
+    imageUrl: '/assets/pupcamp5',
+    label: 'Camping',
+  },
+  {
+    id: 22,
+    imageUrl: '/assets/pupcamp6',
+    label: 'Camping',
+  },
+  {
+    id: 23,
+    imageUrl: '/assets/pupcamp7',
+    label: 'Camping',
+  },
+  {
+    id: 24,
+    imageUrl: '/assets/pupcamp8',
+    label: 'Camping',
+  },
+  {
+    id: 25,
+    imageUrl: '/assets/pupcamp9',
+    label: 'Camping',
+  },
+  {
+    id: 26,
+    imageUrl: '/assets/pupcamp10',
+    label: 'Camping',
+  },
+  {
+    id: 27,
+    imageUrl: '/assets/pupcamp11',
+    label: 'Camping',
+  },
+  {
+    id: 28,
+    imageUrl: '/assets/pupcamp12',
+    label: 'Camping',
+  },
+  {
+    id: 29,
+    imageUrl: '/assets/pupcamp13',
+    label: 'Camping',
+  },
+  {
+    id: 30,
+    imageUrl: '/assets/pupcamp14',
+    label: 'Camping',
+  },
+  {
+    id: 31,
+    imageUrl: '/assets/pupcamp15',
+    label: 'Camping',
+  },
+  {
+    id: 32,
+    imageUrl: '/assets/pupcamp16',
+    label: 'Camping',
+  },
+  {
+    id: 33,
+    imageUrl: '/assets/pupcamp17',
+    label: 'Camping',
+  },
+  {
+    id: 34,
+    imageUrl: '/assets/pupcamp18',
+    label: 'Camping',
+  },
+  {
+    id: 35,
+    imageUrl: '/assets/pupcamp19',
+    label: 'Camping',
+  },
+  {
+    id: 36,
+    imageUrl: '/assets/pupfun1',
+    label: 'Funny',
+  },
+  {
+    id: 37,
+    imageUrl: '/assets/pupfun2',
+    label: 'Funny',
+  },
+  {
+    id: 38,
+    imageUrl: '/assets/pupfun3',
+    label: 'Funny',
+  },
+  {
+    id: 39,
+    imageUrl: '/assets/pupfun4',
+    label: 'Funny',
+  },
+  {
+    id: 40,
+    imageUrl: '/assets/pupfun5',
+    label: 'Funny',
+  },
+  {
+    id: 41,
+    imageUrl: '/assets/pupfun6',
+    label: 'Funny',
+  },
+  {
+    id: 42,
+    imageUrl: '/assets/pupfun7',
+    label: 'Funny',
+  },
+  {
+    id: 43,
+    imageUrl: '/assets/pupfun8',
+    label: 'Funny',
+  },
+  {
+    id: 44,
+    imageUrl: '/assets/pupfun9',
+    label: 'Funny',
+  },
+  {
+    id: 45,
+    imageUrl: '/assets/pupfun10',
+    label: 'Funny',
+  },
+  {
+    id: 46,
+    imageUrl: '/assets/pupfun11',
+    label: 'Funny',
+  },
+  {
+    id: 47,
+    imageUrl: '/assets/pupfun12',
+    label: 'Funny',
+  },
+  {
+    id: 48,
+    imageUrl: '/assets/pupfun13',
+    label: 'Funny',
+  },
+  {
+    id: 49,
+    imageUrl: '/assets/pupfun14',
+    label: 'Funny',
+  },
+  {
+    id: 50,
+    imageUrl: '/assets/pupportrait1',
+    label: 'Portrait',
+  },
+  {
+    id: 51,
+    imageUrl: '/assets/pupportrait2',
+    label: 'Portrait',
+  },
+  {
+    id: 52,
+    imageUrl: '/assets/pupportrait3',
+    label: 'Portrait',
+  },
+  {
+    id: 53,
+    imageUrl: '/assets/pupportrait4',
+    label: 'Portrait',
+  },
+  {
+    id: 54,
+    imageUrl: '/assets/pupportrait5',
+    label: 'Portrait',
+  },
+  {
+    id: 55,
+    imageUrl: '/assets/pupportrait6',
+    label: 'Portrait',
+  },
+  {
+    id: 56,
+    imageUrl: '/assets/pupportrait7',
+    label: 'Portrait',
+  },
+  {
+    id: 57,
+    imageUrl: '/assets/pupportrait8',
+    label: 'Portrait',
+  },
+  {
+    id: 58,
+    imageUrl: '/assets/pupportrait9',
+    label: 'Portrait',
+  },
+  {
+    id: 59,
+    imageUrl: '/assets/pupportrait10',
+    label: 'Portrait',
+  },
+  {
+    id: 60,
+    imageUrl: '/assets/pupportrait11',
+    label: 'Portrait',
+  },
+  {
+    id: 61,
+    imageUrl: '/assets/pupportrait12',
+    label: 'Portrait',
+  },
+  {
+    id: 62,
+    imageUrl: '/assets/pupportrait13',
+    label: 'Portrait',
+  },
+  {
+    id: 63,
+    imageUrl: '/assets/pupportrait14',
+    label: 'Portrait',
+  },
+  {
+    id: 64,
+    imageUrl: '/assets/puppy1',
+    label: 'Puppy',
+  },
+  {
+    id: 65,
+    imageUrl: '/assets/puppy2',
+    label: 'Puppy',
+  },
+  {
+    id: 66,
+    imageUrl: '/assets/puppy3',
+    label: 'Puppy',
+  },
+  {
+    id: 67,
+    imageUrl: '/assets/puppy4',
+    label: 'Puppy',
+  },
+  {
+    id: 68,
+    imageUrl: '/assets/puppy5',
+    label: 'Puppy',
+  },
+  {
+    id: 69,
+    imageUrl: '/assets/puppy6',
+    label: 'Puppy',
+  },
+  {
+    id: 70,
+    imageUrl: '/assets/puppy7',
+    label: 'Puppy',
+  },
+  {
+    id: 71,
+    imageUrl: '/assets/puppy8',
+    label: 'Puppy',
+  },
+  {
+    id: 72,
+    imageUrl: '/assets/puppy9',
+    label: 'Puppy',
+  },
+  {
+    id: 73,
+    imageUrl: '/assets/puppy10',
+    label: 'Puppy',
+  },
+  {
+    id: 74,
+    imageUrl: '/assets/puppy11',
+    label: 'Puppy',
+  },
+  {
+    id: 75,
+    imageUrl: '/assets/puppy12',
+    label: 'Puppy',
+  },
+  {
+    id: 76,
+    imageUrl: '/assets/puppy13',
+    label: 'Puppy',
+  },
+  {
+    id: 77,
+    imageUrl: '/assets/puppy14',
+    label: 'Puppy',
+  },
+  {
+    id: 78,
+    imageUrl: '/assets/puppy15',
+    label: 'Puppy',
+  },
+  {
+    id: 79,
+    imageUrl: '/assets/puppy16',
+    label: 'Puppy',
+  },
+  {
+    id: 80,
+    imageUrl: '/assets/pupsleep1',
+    label: 'Sleeping',
+  },
+  {
+    id: 81,
+    imageUrl: '/assets/pupsleep2',
+    label: 'Sleeping',
+  },
+  {
+    id: 82,
+    imageUrl: '/assets/pupsleep3',
+    label: 'Sleeping',
+  },
+  {
+    id: 83,
+    imageUrl: '/assets/pupsleep4',
+    label: 'Sleeping',
+  },
+  {
+    id: 84,
+    imageUrl: '/assets/pupsleep5',
+    label: 'Sleeping',
+  },
+  {
+    id: 85,
+    imageUrl: '/assets/pupsleep6',
+    label: 'Sleeping',
+  },
+  {
+    id: 86,
+    imageUrl: '/assets/pupsleep7',
+    label: 'Sleeping',
+  },
+  {
+    id: 87,
+    imageUrl: '/assets/pupsleep8',
+    label: 'Sleeping',
+  },
+  {
+    id: 88,
+    imageUrl: '/assets/pupsleep9',
+    label: 'Sleeping',
+  },
+  {
+    id: 89,
+    imageUrl: '/assets/pupsleep10',
+    label: 'Sleeping',
+  },
+  {
+    id: 90,
+    imageUrl: '/assets/pupsleep11',
+    label: 'Sleeping',
+  },
+  {
+    id: 91,
+    imageUrl: '/assets/pupsleep12',
+    label: 'Sleeping',
+  },
+  {
+    id: 92,
+    imageUrl: '/assets/pupsleep13',
+    label: 'Sleeping',
+  },
+  {
+    id: 93,
+    imageUrl: '/assets/pupsleep14',
+    label: 'Sleeping',
+  },
+  {
+    id: 94,
+    imageUrl: '/assets/pupsleep15',
+    label: 'Sleeping',
+  },
+  {
+    id: 95,
+    imageUrl: '/assets/pupsleep16',
+    label: 'Sleeping',
+  },
+  {
+    id: 96,
+    imageUrl: '/assets/pupsleep17',
+    label: 'Sleeping',
+  },
+  {
+    id: 97,
+    imageUrl: '/assets/pupsleep18',
+    label: 'Sleeping',
+  },
+  {
+    id: 98,
+    imageUrl: '/assets/pupsleep19',
+    label: 'Sleeping',
+  },
+  {
+    id: 99,
+    imageUrl: '/assets/pupsleep20',
+    label: 'Sleeping',
+  },
+  {
+    id: 100,
+    imageUrl: '/assets/pupsleep21',
+    label: 'Sleeping',
+  },
+  {
+    id: 101,
+    imageUrl: '/assets/pupsleep22',
+    label: 'Sleeping',
+  },
+  {
+    id: 102,
+    imageUrl: '/assets/pupsleep23',
+    label: 'Sleeping',
+  },
+  {
+    id: 103,
+    imageUrl: '/assets/pupsleep24',
+    label: 'Sleeping',
+  },
+  {
+    id: 104,
+    imageUrl: '/assets/pupsleep25',
+    label: 'Sleeping',
+  },
+  {
+    id: 105,
+    imageUrl: '/assets/pupsleep120',
+    label: 'Sleeping',
+  },
+  {
+    id: 106,
+    imageUrl: '/assets/pupsnow1',
+    label: 'Snow',
+  },
+  {
+    id: 107,
+    imageUrl: '/assets/pupsnow2',
+    label: 'Snow',
+  },
+  {
+    id: 108,
+    imageUrl: '/assets/pupsnow3',
+    label: 'Snow',
+  },
+  {
+    id: 109,
+    imageUrl: '/assets/pupsnow4',
+    label: 'Snow',
+  },
+  {
+    id: 110,
+    imageUrl: '/assets/pupsnow5',
+    label: 'Snow',
+  },
+  {
+    id: 111,
+    imageUrl: '/assets/pupsnow6',
+    label: 'Snow',
+  },
+  {
+    id: 112,
+    imageUrl: '/assets/pupsnow7',
+    label: 'Snow',
+  },
+  {
+    id: 113,
+    imageUrl: '/assets/pupsnow8',
+    label: 'Snow',
+  },
+  {
+    id: 114,
+    imageUrl: '/assets/pupsnow9',
+    label: 'Snow',
+  },
+  {
+    id: 115,
+    imageUrl: '/assets/pupsnow10',
+    label: 'Snow',
+  },
+  {
+    id: 116,
+    imageUrl: '/assets/pupwater1',
+    label: 'Water',
+  },
+  {
+    id: 117,
+    imageUrl: '/assets/pupwater2',
+    label: 'Water',
+  },
+  {
+    id: 118,
+    imageUrl: '/assets/pupwater3',
+    label: 'Water',
+  },
+  {
+    id: 119,
+    imageUrl: '/assets/pupwater4',
+    label: 'Water',
+  },
+  {
+    id: 120,
+    imageUrl: '/assets/pupwater5',
+    label: 'Water',
+  },
+  {
+    id: 121,
+    imageUrl: '/assets/pupwater6',
+    label: 'Water',
+  },
+  {
+    id: 122,
+    imageUrl: '/assets/pupwater7',
+    label: 'Water',
+  },
+  {
+    id: 123,
+    imageUrl: '/assets/pupwater8',
+    label: 'Water',
+  },
+  {
+    id: 124,
+    imageUrl: '/assets/pupwater9',
+    label: 'Water',
+  },
+  {
+    id: 125,
+    imageUrl: '/assets/pupwater10',
+    label: 'Water',
+  },
+  {
+    id: 126,
+    imageUrl: '/assets/pupwater11',
+    label: 'Water',
+  },
+  {
+    id: 127,
+    imageUrl: '/assets/pupwater12',
+    label: 'Water',
+  },
+  {
+    id: 128,
+    imageUrl: '/assets/pupwater13',
+    label: 'Water',
+  },
+  {
+    id: 129,
+    imageUrl: '/assets/pupwater14',
+    label: 'Water',
+  },
+  {
+    id: 130,
+    imageUrl: '/assets/pupwater15',
+    label: 'Water',
+  },
+  {
+    id: 131,
+    imageUrl: '/assets/pupmisc1',
+    label: 'Misc',
+  },
+  {
+    id: 132,
+    imageUrl: '/assets/pupmisc2',
+    label: 'Misc',
+  },
+  {
+    id: 133,
+    imageUrl: '/assets/pupmisc3',
+    label: 'Misc',
+  },
+];
