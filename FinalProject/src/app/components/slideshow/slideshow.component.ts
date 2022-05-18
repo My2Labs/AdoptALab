ac3611d908a43935896b3feb0fe7b71873168e1d
My2Labs <sking171@ford.com>
2022-05-18 14:45:15 -0500

    Add gallery.

--- FinalProject/src/app/components/slideshow/slideshow.component.ts
+++ FinalProject/src/app/components/slideshow/slideshow.component.ts
@@ -1,39 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { photos } from '../../../pup-pictures';
-
-@Component({
-  selector: 'app-slideshow',
-  templateUrl: './slideshow.component.html',
-  styleUrls: ['./slideshow.component.css'],
-})
-export class SlideshowComponent implements OnInit {
-  photos = photos;
-
-  constructor() {}
-
-  ngOnInit(): void {}
-}
-
-// import { GalleryItem, ImageItem } from 'ng-gallery';
+// import { Component, OnInit } from '@angular/core';
+// import { photos } from '../../../pup-pictures';
 
 // @Component({
 //   selector: 'app-slideshow',
 //   templateUrl: './slideshow.component.html',
 //   styleUrls: ['./slideshow.component.css'],
-//   template:
-//   `
-//     <gallery [items]="images"></gallery>
-//   `
 // })
-// export class AppComponent implements OnInit {
+// export class SlideshowComponent implements OnInit {
+//   photos = photos;
 
-//   images: GalleryItem[];
+//   constructor() {}
 
-//   ngOnInit() {
-//     // Set gallery items array
-//     this.images = [
-//       new ImageItem({ src: 'IMAGE_SRC_URL', thumb: 'IMAGE_THUMBNAIL_URL' })),
-//       // ... more items
-//     ];
-//   }
+//   ngOnInit(): void {}
 // }
+import { Component, OnInit } from '@angular/core';
+import { GalleryItem, ImageItem } from 'ng-gallery';
+import { IMAGES } from '../../../puppies';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.css'],
+  template: ` <gallery [items]="images"></gallery> `,
+})
+export class SlideshowComponent implements OnInit {
+  images: GalleryItem[] = [];
+
+  ngOnInit() {
+    // Set gallery items array
+    IMAGES.forEach((item) => {
+      this.images = [
+        ...this.images,
+        new ImageItem({ src: 'item.imageUrl', thumb: 'item.imageUrl' }),
+      ];
+    });
+  }
+}
