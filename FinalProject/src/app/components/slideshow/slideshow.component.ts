200d0ac477e8e12a0d73a7d3b27dac0a4f886116
My2Labs <sking171@ford.com>
2022-05-18 12:58:51 -0500

    Work on slideshow.

--- FinalProject/src/app/components/slideshow/slideshow.component.ts
+++ FinalProject/src/app/components/slideshow/slideshow.component.ts
@@ -13,3 +13,27 @@ export class SlideshowComponent implements OnInit {
 
   ngOnInit(): void {}
 }
+
+// import { GalleryItem, ImageItem } from 'ng-gallery';
+
+// @Component({
+//   selector: 'app-slideshow',
+//   templateUrl: './slideshow.component.html',
+//   styleUrls: ['./slideshow.component.css'],
+//   template:
+//   `
+//     <gallery [items]="images"></gallery>
+//   `
+// })
+// export class AppComponent implements OnInit {
+
+//   images: GalleryItem[];
+
+//   ngOnInit() {
+//     // Set gallery items array
+//     this.images = [
+//       new ImageItem({ src: 'IMAGE_SRC_URL', thumb: 'IMAGE_THUMBNAIL_URL' })),
+//       // ... more items
+//     ];
+//   }
+// }
