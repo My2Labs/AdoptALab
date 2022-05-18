e531291bd4d2cad50f03b8fc24257b6c635ad8a3
My2Labs <sking171@ford.com>
2022-05-18 16:30:23 -0500

    Remove unused components.

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -6,19 +6,13 @@ import { AppComponent } from './app.component';
 import { HeadComponent } from './components/head/head.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { MainpageComponent } from './components/mainpage/mainpage.component';
-import { PuppypicturesComponent } from './components/puppypictures/puppypictures.component';
-import { ActionpicturesComponent } from './components/actionpictures/actionpictures.component';
 import { VideosComponent } from './components/videos/videos.component';
-import { FunnypicturesComponent } from './components/funnypictures/funnypictures.component';
-import { WaterpicturesComponent } from './components/waterpictures/waterpictures.component';
-import { CampingpicturesComponent } from './components/campingpictures/campingpictures.component';
 import { MainsectionComponent } from './components/mainsection/mainsection.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PupsleepComponent } from './components/pupsleep/pupsleep.component';
 import { PortraitsComponent } from './components/portraits/portraits.component';
 import { SlideshowComponent } from './components/slideshow/slideshow.component';
 import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
@@ -32,14 +26,8 @@ import { GALLERY_CONFIG } from 'ng-gallery';
     HeadComponent,
     NavigationComponent,
     MainpageComponent,
-    PuppypicturesComponent,
-    ActionpicturesComponent,
     VideosComponent,
-    FunnypicturesComponent,
-    WaterpicturesComponent,
-    CampingpicturesComponent,
     MainsectionComponent,
-    PupsleepComponent,
     PortraitsComponent,
     SlideshowComponent,
   ],
