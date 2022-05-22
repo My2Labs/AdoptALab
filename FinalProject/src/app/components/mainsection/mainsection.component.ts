dc819a2c7873a07947c205a5d9484ff965aa6664
My2Labs <sking171@ford.com>
2022-05-22 09:03:44 -0500

    Edit form validation.

--- FinalProject/src/app/components/mainsection/mainsection.component.ts
+++ FinalProject/src/app/components/mainsection/mainsection.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-
-type SubscribeData = {
-  firstName: string;
-  lastName: string;
-  email: string;
-};
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mainsection',
@@ -13,18 +7,8 @@ type SubscribeData = {
   styleUrls: ['./mainsection.component.css'],
 })
 export class MainsectionComponent {
-  firstName = new FormControl('', [Validators.required]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  subscribe({ firstName, lastName, email }: SubscribeData) {
-    console.log(firstName, lastName, email);
+  submit(signup: any) {
+    console.log('Form submitted', signup);
   }
 }
 
