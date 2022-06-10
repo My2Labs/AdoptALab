063181fcd8c8c355999de784e19034c0bc65dee9
My2Labs <sking171@ford.com>
2022-06-10 08:06:59 -0500

    Edit search service.

--- FinalProject/src/environments/environment.prod.ts
+++ FinalProject/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   labradorsEndpoint: 'https://labadoption.herokuapp.com/labadoptions',
   randomdogsEndpoint: 'https://dog.ceo/api/breeds/image/random',
-  herokuEndpoint: 'https://labadoption.herokuapp.com/labadoptions/search',
+  herokuEndpoint: 'https://labadoption.herokuapp.com/labadoptions/results',
 };
--- FinalProject/src/environments/environment.ts
+++ FinalProject/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   labradorsEndpoint: 'https://labadoption.herokuapp.com/labadoptions',
   randomdogsEndpoint: 'https://dog.ceo/api/breed/labrador/images',
-  herokuEndpoint: 'https://labadoption.herokuapp.com/labadoptions/search',
+  herokuEndpoint: 'https://labadoption.herokuapp.com/labadoptions/results',
   // randomdogsEndpoint: 'https://api.api-ninjas.com/v1/dogs?name=labrador',
 };
 
