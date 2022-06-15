95d34e78a75029696e6659cb6992fb7a6d1e9fe3
My2Labs <sking171@ford.com>
2022-06-15 14:54:10 -0500

    Add image links.

--- FinalProject/src/puppies.ts
+++ FinalProject/src/puppies.ts
@@ -1,357 +1,428 @@
 export const IMAGES = [
   {
     id: 1,
-    imageUrl: '/assets/pupportrait1.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupportrait1_lzkr1AAEl.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287850474',
     label: 'Portrait',
   },
   {
     id: 2,
-    imageUrl: '/assets/pupportrait2.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupportrait2_-GVOWKovC.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287850566',
     label: 'Portrait',
   },
   {
     id: 3,
-    imageUrl: '/assets/pupportrait3.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupportrait3_hLx36pO6D.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287850533',
     label: 'Portrait',
   },
   {
     id: 4,
-    imageUrl: '/assets/pupportrait4.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupportrait4_N5onQQ9Ma.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287850610',
     label: 'Portrait',
   },
   {
     id: 5,
-    imageUrl: '/assets/pupportrait5.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupportrait5_WqSHCRB5I.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287850798',
     label: 'Portrait',
   },
   {
     id: 6,
-    imageUrl: '/assets/pupwater13.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater13_9Bf1rbpNSR.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044974',
     label: 'Water',
   },
   {
     id: 7,
-    imageUrl: '/assets/pupportrait8.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupportrait8_vr4LLcS2m.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287850880',
     label: 'Portrait',
   },
   {
     id: 8,
-    imageUrl: '/assets/pupportrait9.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupportrait9_pla0ZLax4e.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287850906',
     label: 'Portrait',
   },
   {
     id: 9,
-    imageUrl: '/assets/pupwater12.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater12_Rh56O8aCI.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044974',
     label: 'Water',
   },
   {
     id: 10,
-    imageUrl: '/assets/pupportrait14.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupportrait14_mmlaTvbj8Q.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287850957',
     label: 'Portrait',
   },
   {
     id: 11,
-    imageUrl: '/assets/puppy2.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy2_H3Eli-PWq.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287882216',
     label: 'Puppy',
   },
   {
     id: 12,
-    imageUrl: '/assets/puppy3.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy3_J_F7Pbs4U.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287882257',
     label: 'Puppy',
   },
   {
     id: 13,
-    imageUrl: '/assets/puppy4.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy4_uUB82CN6s.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287882314',
     label: 'Puppy',
   },
   {
     id: 14,
-    imageUrl: '/assets/puppy5.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy5_NZb7Ftm-U.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287882497',
     label: 'Puppy',
   },
   {
     id: 15,
-    imageUrl: '/assets/puppy6.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy6_sdbcbp-28l.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287882598',
     label: 'Puppy',
   },
   {
     id: 16,
-    imageUrl: '/assets/puppy8.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy8_b4l_5Nnmf.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287882639',
     label: 'Puppy',
   },
   {
     id: 17,
-    imageUrl: '/assets/puppy9.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy9_wN9Hwnhs6A.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287883095',
     label: 'Puppy',
   },
   {
     id: 18,
-    imageUrl: '/assets/puppy10.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy10_GguuNI1Xm.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287883165',
     label: 'Puppy',
   },
   {
     id: 19,
-    imageUrl: '/assets/puppy11.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy11_Hqj76TBd9.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287883239',
     label: 'Puppy',
   },
   {
     id: 20,
-    imageUrl: '/assets/puppy15.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy15_oItSamYle.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287883222',
     label: 'Puppy',
   },
   {
     id: 21,
-    imageUrl: '/assets/puppy16.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/puppy16_CTvWeOKNs.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287882355',
     label: 'Puppy',
   },
   {
     id: 22,
-    imageUrl: '/assets/pupaction1.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupaction1_h3pCYSyyg.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287735712',
     label: 'Action',
   },
   {
     id: 23,
-    imageUrl: '/assets/pupaction6.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupaction6_68LK1qrrO.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287735739',
     label: 'Action',
   },
   {
     id: 24,
-    imageUrl: '/assets/pupaction9.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupaction9_meOqvlaBj.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287735963',
     label: 'Action',
   },
   {
     id: 25,
-    imageUrl: '/assets/pupaction10.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupaction10_z705wrGgG.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287736030',
     label: 'Action',
   },
   {
     id: 26,
-    imageUrl: '/assets/pupaction12.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupaction12_6Y9pwEewF.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287735606',
     label: 'Action',
   },
   {
     id: 27,
-    imageUrl: '/assets/pupcamp1.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp1_eETxlsYyr.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287782866',
     label: 'Camping',
   },
   {
     id: 28,
-    imageUrl: '/assets/pupcamp2.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp2_5gsfy30A8.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287782818',
     label: 'Camping',
   },
   {
     id: 29,
-    imageUrl: '/assets/pupcamp3.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp3_KLhZ6-pNO.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287782919',
     label: 'Camping',
   },
   {
     id: 30,
-    imageUrl: '/assets/pupcamp4.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp4_-gyVyxmlo.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287783058',
     label: 'Camping',
   },
   {
     id: 31,
-    imageUrl: '/assets/pupcamp5.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp5_H_envM-ar.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287783209',
     label: 'Camping',
   },
   {
     id: 32,
-    imageUrl: '/assets/pupcamp6.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp6_AhkuwuDr9.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287783261',
     label: 'Camping',
   },
   {
     id: 33,
-    imageUrl: '/assets/pupcamp7.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp7_snX_CbDGc.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287783392',
     label: 'Camping',
   },
   {
     id: 34,
-    imageUrl: '/assets/pupcamp8.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp8_Dvl56WVFP.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287783512',
     label: 'Camping',
   },
   {
     id: 35,
-    imageUrl: '/assets/pupcamp10.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp10_r5cvCUOgN.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287783716',
     label: 'Camping',
   },
   {
     id: 36,
-    imageUrl: '/assets/pupcamp11.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp11_ZUBGm-9oe.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287783717',
     label: 'Camping',
   },
   {
     id: 37,
-    imageUrl: '/assets/pupcamp13.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp13_noRIP7jz1.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287783760',
     label: 'Camping',
   },
   {
     id: 38,
-    imageUrl: '/assets/pupcamp16.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp16_SPhhGJ7--.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287784280',
     label: 'Camping',
   },
   {
     id: 39,
-    imageUrl: '/assets/pupcamp17.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp17_N3m2WNAsE.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287784341',
     label: 'Camping',
   },
   {
     id: 40,
-    imageUrl: '/assets/pupcamp18.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp18_bRY_aAHvX.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287784403',
     label: 'Camping',
   },
   {
     id: 41,
-    imageUrl: '/assets/pupcamp19.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupcamp19_IT8szTAHX.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287782859',
     label: 'Camping',
   },
   {
     id: 42,
-    imageUrl: '/assets/pupfun1.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupfun1_m7ikn9ho4.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287819510',
     label: 'Funny',
   },
   {
     id: 43,
-    imageUrl: '/assets/pupfun6.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupfun6_a6rrc1Mux.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287819435',
     label: 'Funny',
   },
   {
     id: 44,
-    imageUrl: '/assets/pupfun7.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupfun7_4LIwSxOZF.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287819486',
     label: 'Funny',
   },
   {
     id: 45,
-    imageUrl: '/assets/pupfun8.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupfun8_3X8DkAAY5.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287819566',
     label: 'Funny',
   },
   {
     id: 46,
-    imageUrl: '/assets/pupfun9.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupfun9_cWpi0Is7b.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287819742',
     label: 'Funny',
   },
   {
     id: 47,
-    imageUrl: '/assets/pupfun10.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupfun10_KYqlIo4lF.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287819424',
     label: 'Funny',
   },
   {
     id: 48,
-    imageUrl: '/assets/pupsleep12.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsleep12_nOCKbWdet.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287987456',
     label: 'Sleeping',
   },
   {
     id: 49,
-    imageUrl: '/assets/pupsleep17.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsleep17_Q6ltfbj52.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287987490',
     label: 'Sleeping',
   },
   {
     id: 50,
-    imageUrl: '/assets/pupsleep19.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsleep19_QobAILdgy.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287987498',
     label: 'Sleeping',
   },
   {
     id: 51,
-    imageUrl: '/assets/pupsleep21.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsleep21_kJrOQk3a8.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287987607',
     label: 'Sleeping',
   },
   {
     id: 52,
-    imageUrl: '/assets/pupsleep23.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsleep23_tcn8rQI3z.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287987814',
     label: 'Sleeping',
   },
   {
     id: 53,
-    imageUrl: '/assets/pupwater14.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater14_1kDX_HNpx.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044082',
     label: 'Water',
   },
   {
     id: 54,
-    imageUrl: '/assets/pupsleep120.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsleep120_o8cz-zdO8.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655287987442',
     label: 'Sleeping',
   },
   {
     id: 55,
-    imageUrl: '/assets/pupsnow2.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsnow2_9cNHsYGjZ.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288016560',
     label: 'Snow',
   },
   {
     id: 56,
-    imageUrl: '/assets/pupsnow3.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsnow3_nL8ndWntI.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288016564',
     label: 'Snow',
   },
   {
     id: 57,
-    imageUrl: '/assets/pupsnow4.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsnow4_r08hPFT8y.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288016642',
     label: 'Snow',
   },
   {
     id: 58,
-    imageUrl: '/assets/pupsnow5.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsnow5_aTZtx0Acq.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288016850',
     label: 'Snow',
   },
   {
     id: 59,
-    imageUrl: '/assets/pupsnow6.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsnow6_MhvtU_ncR.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288016924',
     label: 'Snow',
   },
   {
     id: 60,
-    imageUrl: '/assets/pupsnow7.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsnow7_GPtHO_xUv.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288016944',
     label: 'Snow',
   },
   {
     id: 61,
-    imageUrl: '/assets/pupsnow8.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsnow8_M6tsXWmNH.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288017173',
     label: 'Snow',
   },
   {
     id: 62,
-    imageUrl: '/assets/pupsnow9.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsnow9_vZzqsg4xG.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288017399',
     label: 'Snow',
   },
   {
     id: 63,
-    imageUrl: '/assets/pupsnow10.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupsnow10_Su0T3ME4E.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288016594',
     label: 'Snow',
   },
   {
     id: 64,
-    imageUrl: '/assets/pupwater1.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater1__jrD6m57H.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044123',
     label: 'Water',
   },
   {
     id: 65,
-    imageUrl: '/assets/pupwater2.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater2_K6NEAOPqE.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044148',
     label: 'Water',
   },
   {
     id: 66,
-    imageUrl: '/assets/pupwater3.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater3_3H9-3Rlyd.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044586',
     label: 'Water',
   },
   {
     id: 67,
-    imageUrl: '/assets/pupwater4.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater4_D8JRSf7Cy.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044576',
     label: 'Water',
   },
   {
     id: 68,
-    imageUrl: '/assets/pupwater5.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater5_YCvmyhrlU.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044562',
     label: 'Water',
   },
   {
     id: 69,
-    imageUrl: '/assets/pupwater8.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater8_opnw5h9dr.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044588',
     label: 'Water',
   },
   {
     id: 70,
-    imageUrl: '/assets/pupwater9.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater9_yJS9Wb2Gl.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044853',
     label: 'Water',
   },
   {
     id: 71,
-    imageUrl: '/assets/pupwater11.jpg',
+    imageUrl:
+      'https://ik.imagekit.io/sharron/pupwater11_g4DYVYoX1.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655288044967',
     label: 'Water',
   },
 ];
